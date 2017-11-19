2992c4c83a1d75c0c974b4d674e7d5eda0ce83e3
Bartosz Legięć <bartosz@legiec.eu>
2017-11-19 19:22:03 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import redis from 'socket.io-redis';
+// import redis from 'socket.io-redis';
 
 import io from './io';
 import queue from './queue';
@@ -7,10 +7,7 @@ import joinRoom from './methods/joinRoom';
 import evaluateEquation from './methods/evaluateEquation';
 import clearQueue from './methods/clearQueue';
 
-let i = 0;
-const steps = [ 'Reaching to 3', 'Reaching to square root', '...', 'Reading from display' ];
-
-io.adapter( redis( { host: 'localhost', port: 6379 } ) );
+// io.adapter( redis( { host: 'localhost', port: 6379 } ) );
 
 io.on( 'connection', function( client ) {
   // client.on( 'disconnect', function( reason ) {
@@ -23,19 +20,4 @@ io.on( 'connection', function( client ) {
   client.on( 'evaluate', evaluateEquation );
 
   client.on( 'clear queue', clearQueue );
-
-  client.on( 'next step', () => {
-    if ( queue[ 'room-pi-1' ] && queue[ 'room-pi-1' ].length > 0 ) {
-      if ( i <= steps.length - 1 ) {
-        io.to( 'room-pi-1' ).emit( 'robot step', steps[ i++ ] );
-        return;
-      }
-
-      queue[ 'room-pi-1' ].shift();
-
-      io.to( 'room-pi-1' ).emit( 'robot done', { result: 46 } );
-      io.to( 'room-pi-1' ).emit( 'queue changed', queue[ 'room-pi-1' ] );
-      i = 0;
-    }
-  } );
 } );
--- src/methods/evaluateEquation.js
+++ src/methods/evaluateEquation.js
@@ -1,6 +1,26 @@
 import io from '../io';
 import queue from '../queue';
 
+let i = 0;
+const steps = [ 'Reaching to 3', 'Reaching to square root', '...', 'Reading from display' ];
+
+const nextStep = () => {
+  if ( queue[ 'room-pi-1' ] && queue[ 'room-pi-1' ].length > 0 ) {
+    if ( i <= steps.length - 1 ) {
+      io.to( 'room-pi-1' ).emit( 'robot step', steps[ i++ ] );
+
+      setTimeout( nextStep, 2000 );
+      return;
+    }
+
+    queue[ 'room-pi-1' ].shift();
+
+    io.to( 'room-pi-1' ).emit( 'robot done', { result: 46 } );
+    io.to( 'room-pi-1' ).emit( 'queue changed', queue[ 'room-pi-1' ] );
+    i = 0;
+  }
+};
+
 export default function( data ) {
   const room = Object.keys( this.adapter.sids[ this.id ] ).filter( r => r !== this.id )[ 0 ];
 
@@ -18,6 +38,8 @@ export default function( data ) {
 
     const rooms = Object.keys( this.adapter.sids[ this.id ] ).filter( r => r !== this.id );
     io.to( rooms ).emit( 'queue changed', queue[ rooms[ 0 ] ] );
+
+    nextStep();
   } else {
     this.emit( 'evaluate error', 'already_submitted' );
   }
