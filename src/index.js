121057b8c2dba2399c6cc9b253cd3bb72beeed6b
Bartosz Legięć <bartosz@legiec.eu>
2017-11-29 22:26:13 +0100

    Fixed typo

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ io.on( 'connection', function( client ) {
       client.emit( 'username given' );
     } else {
       client.emit( 'username error' );
-      console.log( `More than one user tried to cennect with username: ${ username }` );
+      console.log( `More than one user tried to connect with username: ${ username }` );
     }
   } );
 
